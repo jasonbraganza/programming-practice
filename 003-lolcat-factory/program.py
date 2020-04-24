a9812218c6b7b7bd7f6745d01e6b4bd87ce2bf24
Mario Jason Braganza <jason@janusworx.com>
2020-04-24 17:28:34 +0530

    sketched out what i want to do for the lolcat factory

---
+++ 003-lolcat-factory/program.py
@@ -0,0 +1,18 @@
+def main():
+    # print the header
+    # get or create output folder
+    # download cats
+    # display cats
+
+def print_header():
+    """
+    Prints the header for the program
+    """
+    print("============================")
+    print("Welcome to the LOLCAT fatory")
+    print("============================")
+    print()
+
+
+if __name__ == "__main__":
+    pass
