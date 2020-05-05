1d83495fef045ac410a12b12ded16f79e29b9728
Mario Jason Braganza <jason@janusworx.com>
2020-05-05 18:57:09 +0530

    fixed a typo

--- 006-check-system-usage/program.py
+++ 006-check-system-usage/program.py
@@ -6,7 +6,7 @@ def main():
     cpu_usage = psutil.cpu_percent(interval=5)
     mem_usage = psutil.virtual_memory()
     print(
-        f" You are using {cpu_usage}% of your total CPU capacity and {mem_usage[2]}% of total memory, actual - {round((mem_usage[3]/(1024**32)), 2)} GB"
+        f" You are using {cpu_usage}% of your total CPU capacity and {mem_usage[2]}% of total memory, actual - {round((mem_usage[3]/(1024**3)), 2)} GB"
     )
 
 
