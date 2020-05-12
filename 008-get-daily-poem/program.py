bd2aaee78fbff00ea87e04a5e389fdf45d33826e
Mario Jason Braganza <jason@janusworx.com>
2020-05-12 08:07:25 +0530

    sketched out the basic program

---
+++ 008-get-daily-poem/program.py
@@ -0,0 +1,25 @@
+"""
+The idea, is for me to crawl the poetry foundation’s, poem of the day page at
+https://www.poetryfoundation.org/poems/poem-of-the-day and put it into an rss
+file for me, so that i can read it in my RSS reader
+"""
+
+import requests
+
+
+def main():
+    print("hi, you :)")
+    get_page_from_pf()
+    write_to_rss_feed()
+
+
+def get_page_from_pf():
+    pass
+
+
+def write_to_rss_feed():
+    pass
+
+
+if __name__ == "__main__":
+    main()
